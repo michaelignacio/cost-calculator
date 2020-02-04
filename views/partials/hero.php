1fa754bb1fcb6edd13566095449a7ce2008d2de7
Michael Ignacio <mikelignacio@gmail.com>
2020-02-04 10:09:49 +0800

    Update partials folder

---
+++ views/partials/hero.php
@@ -0,0 +1,12 @@
+<section class="hero is-info">
+	<div class="hero-body">
+		<div class="container">
+			<h1 class="title">
+				Cost Calculator
+			</h1>
+			<h2 class="subtitle">
+				Forecast the cost of infrastructure based on customer usage	and expected growth
+			</h2>
+		</div>
+	</div>
+</section>
