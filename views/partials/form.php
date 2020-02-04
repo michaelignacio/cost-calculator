9ddb379e21b2938909d443ab5dadc7624cf57b50
Michael Ignacio <mikelignacio@gmail.com>
2020-02-04 10:57:51 +0800

    Update responsive class

--- views/partials/form.php
+++ views/partials/form.php
@@ -1,7 +1,7 @@
 <section class="section">
 	<div class="container">
 		<form id="form" method="post">
-			<div class="columns">
+			<div class="columns is-desktop">
 				<div class="column">
 					<div class="field">
 						<div class="control">
@@ -23,7 +23,7 @@
 						</div>
 					</div>
 				</div>
-				<div class="column is-one-fifth">
+				<div class="column">
 					<div class="control">
 						<button class="button is-info is-large is-fullwidth" type="submit">Calculate</button>
 					</div>
